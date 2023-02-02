da4b1b08af72e16e1f770c60f7fe836c05148336
Renato Vieira <rvbelo01@gmail.com>
2023-02-01 23:22:39 -0300

    Ajuste Vercel

--- src/features/product/cartSlice.ts
+++ src/features/product/cartSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../../store/store";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../store/store";
 
 interface ItemReduxProps {
   id: number;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { store } from "../store/store";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
 import Globalstyle from "../styles/global-styles";
